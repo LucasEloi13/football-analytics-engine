71f0e5b76aee185a22d3634d177476692a61615d
Lucas Eloi <lucasgev@hotmail.com>
2025-05-29 22:56:11 +0000

    logging_and_request_update

--- scripts/extract/test.py
+++ scripts/extract/test.py
@@ -3,3 +3,10 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
 
 from config.config_loader import load_config
+
+base_url, api_key, config = load_config()
+
+brasileirao_id = config['brasileirao_id']
+
+if not config['brasileirao_id']:
+    config['brasileirao_id'] = 2013
